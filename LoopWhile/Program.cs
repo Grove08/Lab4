5ecf8f8a0abf7639db93c6e0aef4c02c28bebe31
Артём <@COMPUTER>
2023-09-22 10:29:45 +0300

    Добавьте файлы проекта.

---
+++ LoopWhile/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoopWhile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int A = 0;
+            int B = 0;
+
+            int C;
+            do
+            {
+                Console.WriteLine("Введите число (0 для завершения): ");
+                C = Convert.ToInt32(Console.ReadLine());
+                if (C > 0)
+                    A++;
+                else if (C < 0)
+                    B++;
+            }                                      
+            while (C !=0);
+            Console.WriteLine("Количество положительных чисел: " + A);
+            Console.WriteLine("Количество отрицательных чисел: " + B);
+            if (A > B)
+                Console.WriteLine("Положительные чисел больше");
+            else if (A < C)
+                Console.WriteLine("Отрицательных чисел больше");
+            else
+                Console.WriteLine("Количество положительных и отрицательных чисел равно");
+            Console.ReadKey();
+        }
+    }
+}
